35aad129e531fa82e70a4c83172d6b672fbddbe7
‘HarshitChejara’ <harshitchejara29@gmail.com>
2024-04-24 19:48:55 +0530

    changes

--- src/app/components/Faq/index.tsx
+++ src/app/components/Faq/index.tsx
@@ -25,6 +25,7 @@ export default function Faq() {
                             </label>
                             <span className="text-black mr-3 inline-flex items-center ml-auto leading-none text-sm pr-3">
                                 <label htmlFor={`faq-${index}`}>
+                                <svg className={`w-5 h-5 ml-auto inline-flex ${expandedId === index ? 'rotate-180' : ''}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>
                                     {/* <FontAwesomeIcon icon={faChevronDown} className={`w-5 h-5 ml-auto inline-flex ${expandedId === index ? 'rotate-180' : ''}`} /> */}
                                 </label>
                             </span>
